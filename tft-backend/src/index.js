3eba889c31ea5eab1d35767c212fd0687fe2b0b2
SungJin-Jin <deskhi@nethru.co.kr>
2019-12-08 16:46:07 +0900

    [TA-#5] Add mongoose env

--- tft-backend/src/index.js
+++ tft-backend/src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const Koa = require('koa');
 const Router = require('koa-router');
 
@@ -5,11 +7,23 @@ const app = new Koa();
 const router = new Router();
 const api = require('./api');
 
+const moongoose = require('mongoose');
+
+moongoose.Promise = global.Promise;
+moongoose.connect(process.env.MONGO_URI, {
+    useMongoClient: true
+}).then(() => {
+    console.log('Successfully connected to mongodb');
+}).catch((e) => {
+    console.error(e);
+});
+
+const port = process.env.PORT || 4000;
+
 router.use('/api', api.routes());
 
-app.use(router.routes())
-    .use(router.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-    console.log('Started server...');
+app.listen(port, () => {
+    console.log('listening to port', port);
 });
