db42021cd3913399fef91c345ad2b581bf1a8546
SungJin-Jin <deskhi@nethru.co.kr>
2019-12-11 17:36:37 +0900

    [CN-#10] Add Unit Card

---
+++ tft-frontend/src/views/UnitCard.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {Card, CardBody, CardFooter, CardImg, Button} from "reactstrap";
+
+class UnitCard extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Card className="card-user card">
+                <CardBody className="card-body">
+                    <p className="card-text"></p>
+                    <div className="author">
+                        <div className="block block-one"></div>
+                        <div className="block block-two"></div>
+                        <div className="block block-three"></div>
+                        <div className="block block-four"></div>
+                        <CardImg alt="..." className="avatar"
+                                 src={this.props.image}/>
+                        <h3 className="title">Top {this.props.rank}</h3>
+                        <p className="description">{this.props.name}</p>
+                    </div>
+                    <div className="card-description text-center">
+                       Tier : {this.props.tier}
+                    </div>
+                </CardBody>
+                <CardFooter className="card-footer">
+                    <div className="button-container">
+                        <Button className="btn-icon btn-round btn btn-facebook"><i className="fab fa-facebook"></i>
+                        </Button>
+                        <Button className="btn-icon btn-round btn btn-twitter"><i className="fab fa-twitter"></i>
+                        </Button>
+                        <Button className="btn-icon btn-round btn btn-google"><i className="fab fa-twitter"></i>
+                        </Button>
+                    </div>
+                </CardFooter>
+            </Card>
+
+        )
+    }
+}
+
+UnitCard.defaultProps = {
+  image: "",
+  rank: 0,
+  name: "NONE",
+  tier: 0
+};
+
+export default UnitCard;
